3b6b34325ec7004c7fb74d68f9cbd658fdae4dc7
Isaac Dream <isaackprogramming44524d@gmail.com>
2026-03-31 20:43:49 +0200

    added routes for html files

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app_instance = Flask(__name__)
 def index():
     return render_template('index.html', title = 'Home', year=get_year())
 
+@app_instance.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app_instance.route('/data')
+def data():
+    return render_template('data.html', title='Data source')
+
 def get_year():
     current_year = date.today().year
     return current_year
