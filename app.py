285590a31698aa3cdbf82aa13bea5c0897ac5961
Isaac Dream <isaackprogramming44524d@gmail.com>
2026-04-01 06:11:17 +0200

    files

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app_instance = Flask(__name__)
 
 @app_instance.route('/')
 def index():
-    return render_template('index.html', title = 'Home', year=get_year())
+    return render_template('index.html', title = 'Home', year=get_year(), active_page='index')
 
 @app_instance.route('/about')
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html', title='About', active_page='about')
 
 @app_instance.route('/data')
 def data():
-    return render_template('data.html', title='Data source')
+    return render_template('data.html', title='Data source', active_page=data)
 
 def get_year():
     current_year = date.today().year
